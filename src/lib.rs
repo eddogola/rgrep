b8c348b24cd02883c5e9a23dcb293a47ade32b0a
Eddy Ogola <eddy.ogola1@gmail.com>
2022-06-16 06:06:17 -0700

    Implement search case insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,27 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
+    let (query_lower, contents_lower) = (query.to_lowercase(), contents.to_lowercase());
+    
+    // got an error referencing above variables to use in search
+    // so I'm going to write a replica of search to get the test to pass
+    // then refactor later
+    let contents: Vec<&str> = contents.lines().collect();
+    let mut results = Vec::new();
+    let mut index = 0;
+    for line in contents_lower.lines() {
+        if line.contains(&query_lower) {
+            // results.push(String::from(line));
+            println!("{}", index);
+            results.push(contents[index]);
+        }
+        index += 1;
+    }
+
+    results
+}
+
 #[cfg(test)]
 pub mod tests {
 
@@ -56,6 +77,7 @@ Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
+    #[test]
     fn case_insensitive_search() {
         let query = "rUsT";
         let contents = "\
