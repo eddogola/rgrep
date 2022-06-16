4b933cca9e14a9424e2e2df76b397f5acba2ebad
Eddy Ogola <eddy.ogola1@gmail.com>
2022-06-16 05:17:45 -0700

    Write test for case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,7 @@ pub mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive_search() {
         let query = "tive";
         let contents = "\
 Rust:
@@ -54,6 +54,16 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    fn case_insensitive_search() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 
 }
